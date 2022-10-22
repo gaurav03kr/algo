64a9893e61f2461396fa0ab6b2f27af5c3de10db
prableensingh <116462627+prableensingh@users.noreply.github.com>
2022-10-23 01:55:26 +0530

    Create kadane'salgorithm.cpp
    Signed-off-by: prableensingh <116462627+prableensingh@users.noreply.github.com>

---
+++ kadane'salgorithm.cpp
@@ -0,0 +1,51 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long maxsum=arr[0];
+       long currsum=0;
+        for(int i=0;i<n;i++)
+        {
+            currsum+=arr[i];
+        
+        if(currsum>maxsum)
+        {
+            maxsum=currsum;
+        }
+        if(currsum<0)
+        {
+            currsum=0;
+        }
+        
+    }
+    return maxsum;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
