170d80aa83c1f56e8648cf593ad4167a365bdb95
gaurav03kr <109639032+gaurav03kr@users.noreply.github.com>
2022-10-24 11:49:58 +0530

    Revert "Revert "Create sparse_table.cpp""

---
+++ sparse_table.cpp
@@ -0,0 +1,31 @@
+int st[LIM][22], lg[LIM];
+
+int combine(int a, int b)
+{
+    return min(a, b);
+}
+
+void build(int n)
+{
+    lg[1] = 0;
+    for (int i = 2; i < LIM; ++i)
+        lg[i] = lg[i / 2] + 1;
+
+    for (int i = 0; i < n; ++i)
+        st[i][0] = arr[i];
+
+    for (int j = 1; j < 22; ++j)
+    {
+        for (int i = 0; i + (1 << j) <= n; ++i)
+            st[i][j] = combine(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+    }
+}
+
+int query(int l, int r)
+{
+    int j = lg[r - l + 1];
+    int ans = combine(st[l][j], st[r - (1 << j) + 1][j]);
+
+    return ans;
+}
+Footer
